ccc7819e4595e7d8ff143e05172797dab420fc5d
Aryan Sharma <103247130+Aryan-004@users.noreply.github.com>
2022-04-08 19:26:02 +0530

    Add files via upload

--- git0.py
+++ git0.py
@@ -6,4 +6,17 @@ Created on Fri Apr  8 12:58:15 2022
 @author: aryan
 """
 
+def weird_algo(n):
+    while n>1:
+        if n%2==0:
+            n=n/2
+            n=int(n)
+            print(n)
+        else:
+            n=(n*3)+1
+            print(n)
+n=int(input())
+result=weird_algo(n)
+print(result,end=' ')
+
 print("hello git")
